d6347ac4c4951a15feb6fa32f848e3bc76d17836
Mattias <mattiasleungis@gmail.com>
2023-03-06 16:04:01 -0800

    forgot to add to github repesitory

--- js /script.js
+++ js /script.js
@@ -1,16 +1,29 @@
-
 const courseList = [
-    { code: "ACIT 1620", name: "Web Fundamental Technologies" },
-    { code: "ACIT 1630", name: "Database Systems" },
-    { code: "ACIT 1420", name: "Introduction to System Administration" }
-  ];
-  
+  { code: "ACIT 1620", name: "Web Fundamental Technologies" },
+  { code: "ACIT 1630", name: "Database Systems" },
+  { code: "ACIT 1420", name: "Introduction to System Administration" },
+];
 
-  let courseCode = prompt("Please enter a 4-digit course code:");
-  while (isNaN(courseCode) || courseCode.toString().length !== 4) {
-    courseCode = prompt("Please enter a valid 4-digit course code:");
+let courseCode = prompt("Please enter a 4-digit course code:");
+while (isNaN(courseCode) || courseCode.toString().length !== 4) {
+  courseCode = prompt("Please enter a valid 4-digit course code:");
+}
+let courseFound = false;
+for (let i = 0; i < courseList.length; i++) {
+  if (courseList[i].code.includes(courseCode)) {
+    console.log("Yes, I am taking course: " +  courseList[i].code + " - " + courseList[i].name);
+    courseFound = true;
+    break;
   }
-  
+}
 
-  console.log(courseList);
-  
+if (!courseFound) {
+  const newCourse = {
+    code: "ACIT " + courseCode,
+    name: null,
+  };
+  courseList.push(newCourse);
+  console.log(`Added new course with code: ACIT ${courseCode}`);
+}
+
+console.log(courseList);
